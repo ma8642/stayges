ac8df92f6c585441be23679b2c69cf8d1865faca
Marley Alford <marleycalford@gmail.com>
2022-08-17 13:47:05 -0400

    Added images for about page

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "74f3da49732ff552"; }
+/******/ 		__webpack_require__.h = function() { return "fca9e1679a483723"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
